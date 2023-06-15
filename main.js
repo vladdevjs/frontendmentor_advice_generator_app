e00f99387940b1d970990e798153d0bbe0551fcf
Vladislav <vladdevs@yandex.ru>
2023-06-15 20:20:07 +0300

    feat: Release

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const button = document.querySelector('.next_advice');
 const getAdvice = async () => {
   const res = await fetch('https://api.adviceslip.com/advice');
   const data = await res.json();
-  number.textContent = data.slip.id;
+  number.textContent = `Advice #${data.slip.id}`;
   advice.textContent = data.slip.advice;
 };
 
