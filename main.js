baef5e97d8f34891d8916a9f249f76fd7016ca03
Vladislav <vladdevs@yandex.ru>
2023-06-15 17:16:13 +0300

    Feat: get quote

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = 'https://api.adviceslip.com/advice';
-
 const number = document.querySelector('.advice_number');
 const advice = document.querySelector('.advice');
 const button = document.querySelector('.next_advice');
 
-function getAdvice() {
-  return fetch(apiUrl, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      console.log(data.slip.id);
-      number.textContent = data.slip.id;
-      advice.textContent = data.slip.advice;
-    });
-}
+const getAdvice = async () => {
+  const res = await fetch('https://api.adviceslip.com/advice');
+  const data = await res.json();
+  number.textContent = data.slip.id;
+  advice.textContent = data.slip.advice;
+};
 
 getAdvice();
 
