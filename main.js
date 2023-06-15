56fc1b60630cfe0665c91bd825ecd5f52202763a
Vladislav <vladdevs@yandex.ru>
2023-06-15 17:26:10 +0300

    Feat: get quote

--- main.js
+++ main.js
@@ -3,12 +3,7 @@ const advice = document.querySelector('.advice');
 const button = document.querySelector('.next_advice');
 
 const getAdvice = async () => {
-  const res = await fetch('https://api.adviceslip.com/advice', {
-    headers: {
-      'Cache-Control': 'no-cache',
-      Pragma: 'no-cache',
-    },
-  });
+  const res = await fetch('https://api.adviceslip.com/advice');
   const data = await res.json();
   number.textContent = data.slip.id;
   advice.textContent = data.slip.advice;
